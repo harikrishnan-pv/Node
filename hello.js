ba390f1aad6efe35436d859b3aa07100f885e91c
Mr. Robot <harikrznan@gmail.com>
2021-08-30 18:36:07 +0530

    Local server up

--- hello.js
+++ hello.js
@@ -1,3 +1,13 @@
-var read=require('readline-sync')
-var value= read.question('Enter the number')
-console.log(value)
+var http=require('http')
+var fs=require('fs')
+
+http.createServer(function(req,res){
+    fs.readFile('index.html', function(error,data){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write(data)
+        res.end()
+    }
+    
+    
+    )
+}).listen(7000)
